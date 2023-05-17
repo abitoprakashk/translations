6ba9c37b6b0f5cd4ddb46b92342bb7d96da98fa8
abitoprakash <abito@teachmint.com>
2023-05-17 18:55:52 +0530

    asd

--- src/components/AdminPage/AddAdminPopUp.js
+++ src/components/AdminPage/AddAdminPopUp.js
@@ -15,7 +15,7 @@ export default function AddAdminPopUp({
   setShowAcknowledgementPopup,
   setValidationObject,
 }) {
-  // YOLO
+  // YOLO s
   const [adminName, setAdminName] = useState('')
   const [countryCode, setCountryCode] = useState('91')
   const [phoneNumber, setPhoneNumber] = useState('')
