350bc4919306fdadd809ce4cece2356aaff0a199
abitoprakash <abito@teachmint.com>
2023-05-17 19:08:08 +0530

    Dix

--- src/components/InstituteInfoIncompleteNotice/InstituteInfoIncompleteNotice.constants.js
+++ src/components/InstituteInfoIncompleteNotice/InstituteInfoIncompleteNotice.constants.js
@@ -2,5 +2,5 @@ export const NOTICE = {
   CERTIFICATES: 'Certificates',
   GENERATE_CERTIFICATE: 'Generate Certificate',
   INSTITUTE_ADDRESS: 'Institute Address',
-  INSTITUTE_LOGO: 'Institute Logo',
+  INSTITUTE_LOGO: 'Institute Logos',
 }
